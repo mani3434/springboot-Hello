c201cf58c3131a2f76d0181b89a8d26a51056aaa
mani3434 <S528120@mail.nwmissouri.edu>
2018-05-03 10:07:43 -0400

    springboot starter project updated

---
+++ src/main/java/com/fund/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.fund.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hi")
+	public String sayHello() {
+		
+		return "All the best";
+	}
+	
+	
+	
+	
+}
